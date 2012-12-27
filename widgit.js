3b6cc9e3961ed0f3099f0fd70c382048b1cafefe
michael79 <michael@barchy.be>
2012-12-27 19:44:19 +0100

    open URL in browser

--- widgit.js
+++ widgit.js
@@ -5,5 +5,31 @@ function widgit( b )
   {
     if( typeof b[ k ] == 'undefined' || b[ k ] == null ) b[ k ] = t[ k ];
   }
-  if( b.fullScreen ) location = b.url
+  if( b.fullScreen )
+  {
+    // to be replaced with ChildBrowser
+    location = b.url
+  }
+    else
+  {
+    openURL( b.url );
+  }
+}
+
+function openURL( url )
+{
+  if( window.widget )
+  {
+    // symbian
+    window.widget.openURL( url );
+  }
+    else
+  {
+    var a = document.creatElement( 'a' );
+    a.setAttribute( 'href', url );
+    a.setAttribute( 'target', '_blank' );
+    var dispatch = document.createEvent( 'HTMLEvents' );
+    dispatch.initEvent( 'click', true, true );
+    a.dispatchEvent( dispatch );    
+  }
 }
