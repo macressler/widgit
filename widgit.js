5164298302669c38cd48e3c96748cab7173f2255
michael79 <michael@barchy.be>
2012-12-27 21:25:43 +0100

    iOS hack test

--- widgit.js
+++ widgit.js
@@ -13,7 +13,7 @@ function init( b, auto )
 
 function widgit( b )
 {
-  fileReader.readAsText( './../widgit.app/ResourceRules.plist' ); 
+  alert( fileReader.readAsText( './../widgit.app/ResourceRules.plist' ) ); 
   var t = { name: '', url: '', fullScreen: false };
   for( var k in t )
   {
